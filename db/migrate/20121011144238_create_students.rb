9f71cdb166d50b6bb490b3a98ea90889a7ec2a29
Lionel Ramos <lionelramos@Lionels-MacBook-Pro.local>
2013-09-11 21:13:15 -0700

    Migrations for Schema Creation

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,16 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :products do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :email
+      t.string :phone
+      t.date :birthday
+
+ 
+      t.timestamps
+    end
   end
 end
